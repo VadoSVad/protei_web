6e4f2421c9b2af5c81b948c51df3f5a351c1cbbb
VadoSVad <vadimsol@yandex.ru>
2023-12-06 17:53:56 +0300

    Добавление модулей monorepo, а также установка yarn, webpack, typescript

---
+++ packages/module_1/src/index.ts
@@ -0,0 +1,5 @@
+const greetModule1 = () => {
+    console.log('Hello from Module 1!');
+};
+
+export default greetModule1;
---
+++ packages/module_2/src/index.ts
@@ -0,0 +1,5 @@
+const greetModule2 = () => {
+    console.log('Hello from Module 2!');
+};
+
+export default greetModule2;
